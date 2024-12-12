db50b4915ffb1b2365839d74df423ecfb02e092b
yvan7123 <yangfan7123@gmail.com>
2024-12-12 10:08:44 +0800

    f

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TreeTransferTest from './components/TreeTransferTest.vue';
+import ChildTest  from './components/ChildTest.vue';
+
+import {ref} from 'vue';
+const child = ref("abc");
+</script>
+
+<template>
+  <tree-transfer-test></tree-transfer-test>
+  <child-test v-model="child"></child-test>
+  <div>测试：</div>
+  {{ child }}
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  msg: String,
+})
+
+console.log(props.msg)
+const count = ref(0)
+count.value ++ 
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <div>{{ count }}</div>
+  </div>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
