25fa1d9dcd95156dc60832a61d8942901232f483
Yvan7123_ <yangfan7123@gmail.com>
2024-12-25 14:29:29 +0800

    -

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -3,5 +3,6 @@ import components from "./components"
 import "@yvan-element/theme/index.css"
 const installer = makeInstaller(components);
 
+
 export * from "@yvan-element/components";
 export default installer;
