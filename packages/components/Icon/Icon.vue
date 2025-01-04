5a2e26968d65bf735aba0b3c0c884441f6c159d7
yvan7123 <yangfan7123@gmail.com>
2025-01-04 22:33:33 +0800

    01040

---
+++ packages/components/Icon/Icon.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  import type { IconProps } from './types'
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+  import { computed } from 'vue'
+  import { omit } from 'lodash-es'
+  defineOptions({
+    name: 'ErIcon',
+    inheritAttrs: false,
+  })
+
+  const props = defineProps<IconProps>()
+  const filterProps = computed(() => omit(props, ['type', 'color']))
+  const customStyles = computed(() => ({ color: props.color ?? void 0 }))
+</script>
+
+<template>
+  <i
+    class="er-icon"
+    :class="[`er-icon-${props.type}`]"
+    :style="customStyles"
+    v-bind="$attrs"
+  >
+    <font-awesome-icon v-bind="filterProps" />
+  </i>
+</template>
+
+<styple scoped>
+  @import "./style.css"
+</styple>
